d5247d53e69eed0d877147e91858900696b62595
Francois Marais <francois.m@superbalist.com>
2024-03-06 19:07:24 +0200

    add files, html bootstra with theme freelancer

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+// set current year string at the bottom of the page
+const date = new Date();
+let year = date.getFullYear();
+// Copyright © Ads Roll 2024
+document.getElementById("copyright_and_year").innerHTML = `Copyright © Ads Roll ${year}`;
